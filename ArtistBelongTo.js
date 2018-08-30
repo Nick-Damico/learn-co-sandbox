67a38928743528d29be6860e834809c10e6337cf
Nicholas D'Amico <nickalan82@icloud.com>
2018-08-30 01:27:51 +0000

    add methods for associating Models with a 'has many' relationship

--- ArtistBelongTo.js
+++ ArtistBelongTo.js
@@ -1,16 +1,41 @@
 class Artist {
   constructor(name) {
     this._name = name;
+    this._songs = [];
   }
   
   getName() {
     return this._name;
   }
+  
+  addSong(song) {
+    if(!this._songs.includes(song)) {
+      this._songs.push(song)
+    } else {
+      console.warn( "Song was already added" );
+    }
+    if(!song.getArtist()) {
+      song.setArtist(this);
+    }
+  }
+  
+  getSongs() {
+    return this._songs;
+  }
+  
+  listSongs() {
+    console.log(`${this.getName()}'s Songs`);
+    console.log('-------------------------');
+    for(let song of this._songs) {
+      console.log(song.getTitle());
+    }
+  }
 }
 
 class Song {
-  constructor(title) {
+  constructor(title,genre) {
     this._title = title;
+    this._genre = genre;
   }
   
   getTitle() {
@@ -25,12 +50,18 @@ class Song {
   getArtist() {
     return this._artist;
   }
+  
 }
 
 let drake = new Artist('Drake');
 console.log(drake.getName());
 
-let hotlineBling = new Song('Hotline Bling');
+let hotlineBling = new Song('Hotline Bling', 'Rap');
+let fireworks = new Song('Fireworks', 'R&B');
+
 hotlineBling.setArtist(drake);
 
-console.log(hotlineBling.getArtist().getName());
+drake.addSong(hotlineBling);
+drake.addSong(fireworks);
+console.log(`Artist of ${hotlineBling.getTitle()}: ${fireworks.getArtist().getName()}`);
+drake.listSongs();
